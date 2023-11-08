66cd132dfdf452eb42699cc758fc34782e770971
Nattapat Wakhet <nattapat.wakhet.2505@gmail.com>
2023-11-08 10:52:59 +0700

    start

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+// const { createServer } = require('node:http');
+// const { join } = require('node:path');
+const { Server } = require("socket.io");
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const messages = []
+
+io.on('connection', (socket) => {
+//   const username = socket.handshake.query.username
+//   console.log(username);
+  socket.on('message', (data) => {
+    const message = {
+      message: data.message,
+      usernamesend: data.usernamesend,
+      sentdate: new Date()
+    }
+    messages.push(message)
+    io.emit('message', message)
+  })
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
